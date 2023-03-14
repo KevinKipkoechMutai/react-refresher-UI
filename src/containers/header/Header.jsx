6aa37920512b5be07a9da32e6af7c8ed91ad1d91
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-03-14 16:58:20 +0300

    work on header/home section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,22 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>
+          Let's Build Something Amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Amazing AI products can be generated through GPT-3 technology.
+           There is a wide range of applications just waiting to be uncovered by you!
+            Try us today and open the door to endless opportunities. 
+        </p>
+        <div className='gpt3__header-content__input'>
+          <input type='email' placeholder='Your email address'/>
+          <button type='button'>Get started</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
