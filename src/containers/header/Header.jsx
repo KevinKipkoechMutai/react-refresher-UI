401775b84f8ef4086a8e242b034ac10edc4a209e
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-03-17 18:24:30 +0300

    build and style header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import people from "../../assets/people.png"
+import ai from "../../assets/ai.png"
 
 const Header = () => {
   return (
@@ -17,6 +19,14 @@ const Header = () => {
           <input type='email' placeholder='Your email address'/>
           <button type='button'>Get started</button>
         </div>
+
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="people"/>
+          <p>1600 people requested access in the last 24 hours</p>
+        </div> 
+      </div>
+      <div className='gpt3__header-image'>
+          <img src={ai} alt="ai"/>
       </div>
     </div>
   )
