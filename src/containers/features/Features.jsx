2717d7061190452fb60d7c4aa23898796d6af1c7
Kevin Kipkoech <mutaikevinkip.05@gmail.com>
2023-03-17 19:04:00 +0300

    create and style features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import './features.css'
+import { Feature } from '../../components'
+
+const featuresData = [
+  {
+    title: "Improving end distrusts instantly",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+  },
+  {
+    title: "Become the tended active",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+  },
+  {
+    title: "Message or am nothing",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+  },
+  {
+    title: "Really boy law county",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+  }
+]
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>The Future is Now and You Just Need to Realize it. Step into Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={item.index} />
+        ))}
+      </div>
+    </div>
   )
 }
 
